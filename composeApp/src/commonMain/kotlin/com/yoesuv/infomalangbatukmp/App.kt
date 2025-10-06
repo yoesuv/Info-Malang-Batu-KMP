34fec2ba65c5c8487de960082152081a4264e476
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-10-07 05:42:26 +0700

    Implements splash screen and navigation
    Sets up the initial splash screen and configures navigation using Compose Navigation.
    
    This enhancement introduces a splash screen that is displayed for a short duration before navigating to the home screen.
    It defines app routes and utilizes a NavHost to manage navigation between the splash screen and other features.

--- composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/App.kt
@@ -1,48 +1,28 @@
 package com.yoesuv.infomalangbatukmp
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yoesuv.infomalangbatukmp.core.route.AppRoute
+import com.yoesuv.infomalangbatukmp.feature.home.HomeScreen
+import com.yoesuv.infomalangbatukmp.feature.splash.SplashScreen
 
-import infomalangbatukmp.composeapp.generated.resources.Res
-import infomalangbatukmp.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.material3.MaterialTheme
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = AppRoute.Splash
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            composable<AppRoute.Splash> {
+                SplashScreen(navController)
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            composable<AppRoute.Home> {
+                HomeScreen()
             }
         }
     }
--- composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/feature/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/feature/splash/SplashScreen.kt
@@ -3,16 +3,37 @@ package com.yoesuv.infomalangbatukmp.feature.splash
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.yoesuv.infomalangbatukmp.core.route.AppRoute
+import infomalangbatukmp.composeapp.generated.resources.Res
+import infomalangbatukmp.composeapp.generated.resources.app_name
+import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        navController.navigate(AppRoute.Home) {
+            popUpTo(AppRoute.Splash) { inclusive = true }
+        }
+    }
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "Info Malang Batu")
+        Text(
+            text = stringResource(Res.string.app_name), style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
     }
 }
