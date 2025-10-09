2e5bab3779fe9ffbcc95edc9da0ce8f30c65323c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-10-10 06:20:30 +0700

    Updates app theme colors
    Updates the application's theme colors to improve the user
    interface aesthetics and consistency. It modifies the
    primary, primaryContainer, onPrimaryContainer,
    secondaryContainer and onSecondaryContainer colors.

--- composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/App.kt
@@ -9,13 +9,18 @@ import com.yoesuv.infomalangbatukmp.feature.home.HomeScreen
 import com.yoesuv.infomalangbatukmp.feature.splash.SplashScreen
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.Color
 import com.yoesuv.infomalangbatukmp.core.theme.AppColors
 
 @Composable
 fun App() {
     MaterialTheme(
         colorScheme = MaterialTheme.colorScheme.copy(
-            primary = AppColors.Primary
+            primary = AppColors.Primary,
+            primaryContainer = AppColors.Primary,
+            onPrimaryContainer = Color.White,
+            secondaryContainer = AppColors.Primary.copy(alpha = 0.3f),
+            onSecondaryContainer = AppColors.Primary
         )
     ) {
         val navController = rememberNavController()
