5a32ea08b1a34af91843576112b57754636677c7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-11-01 15:30:04 +0700

    feat: add additional libraries to About libraries list
    Adds several library entries (Coil, kotlinx-serialization, Jsoup, Ktor) to the About screen's libraries list and wires their localized names, URLs, and license strings into the view model. Enables display of these dependencies and their license info in the app, improving transparency about bundled third-party components.

--- composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/feature/about/LibrariesViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/feature/about/LibrariesViewModel.kt
@@ -5,9 +5,21 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.yoesuv.infomalangbatukmp.core.models.LibraryModel
+import infomalangbatukmp.composeapp.generated.resources.coil
+import infomalangbatukmp.composeapp.generated.resources.coil_license
+import infomalangbatukmp.composeapp.generated.resources.coil_url
 import infomalangbatukmp.composeapp.generated.resources.compose_multiplatform
 import infomalangbatukmp.composeapp.generated.resources.compose_multiplatform_license
 import infomalangbatukmp.composeapp.generated.resources.compose_multiplatform_url
+import infomalangbatukmp.composeapp.generated.resources.kotlinx_serialization
+import infomalangbatukmp.composeapp.generated.resources.kotlinx_serialization_license
+import infomalangbatukmp.composeapp.generated.resources.kotlinx_serialization_url
+import infomalangbatukmp.composeapp.generated.resources.ksoup
+import infomalangbatukmp.composeapp.generated.resources.ksoup_license
+import infomalangbatukmp.composeapp.generated.resources.ksoup_url
+import infomalangbatukmp.composeapp.generated.resources.ktor
+import infomalangbatukmp.composeapp.generated.resources.ktor_license
+import infomalangbatukmp.composeapp.generated.resources.ktor_url
 import infomalangbatukmp.composeapp.generated.resources.Res.string as str
 
 class LibrariesViewModel : ViewModel() {
@@ -26,6 +38,30 @@ class LibrariesViewModel : ViewModel() {
                     url = str.compose_multiplatform_url,
                     license = str.compose_multiplatform_license,
                     isLast = false
+                ),
+                LibraryModel(
+                    name = str.coil,
+                    url = str.coil_url,
+                    license = str.coil_license,
+                    isLast = false
+                ),
+                LibraryModel(
+                    name = str.kotlinx_serialization,
+                    url = str.kotlinx_serialization_url,
+                    license = str.kotlinx_serialization_license,
+                    isLast = false
+                ),
+                LibraryModel(
+                    name = str.ksoup,
+                    url = str.ksoup_url,
+                    license = str.ksoup_license,
+                    isLast = false
+                ),
+                LibraryModel(
+                    name = str.ktor,
+                    url = str.ktor_url,
+                    license = str.ktor_license,
+                    isLast = true
                 ),
             )
         )
