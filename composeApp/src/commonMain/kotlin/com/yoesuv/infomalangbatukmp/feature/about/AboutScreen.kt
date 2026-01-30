16c5fb9821a96debda2f2f40d70e25f2f928e8af
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2026-01-30 15:15:08 +0700

    refactor: Improves tab row appearance
    Updates the tab row to use PrimaryScrollableTabRow for better visual consistency.
    
    Adjusts the tab indicator for improved styling and customization.
    Removes unnecessary imports and simplifies the tab row implementation.

--- composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/feature/about/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yoesuv/infomalangbatukmp/feature/about/AboutScreen.kt
@@ -1,21 +1,19 @@
 package com.yoesuv.infomalangbatukmp.feature.about
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.PrimaryScrollableTabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import infomalangbatukmp.composeapp.generated.resources.Res
@@ -37,20 +35,21 @@ fun AboutScreen() {
     val coroutineScope = rememberCoroutineScope()
 
     Column(modifier = Modifier.fillMaxSize()) {
-        ScrollableTabRow(
+        PrimaryScrollableTabRow(
             edgePadding = 0.dp,
             selectedTabIndex = pagerState.currentPage,
             modifier = Modifier.fillMaxWidth(),
             containerColor = MaterialTheme.colorScheme.primary,
             contentColor = MaterialTheme.colorScheme.surface,
-            indicator = { tabPositions ->
-                Box(
-                    modifier = Modifier
-                        .tabIndicatorOffset(tabPositions[pagerState.currentPage])
-                        .height(5.dp)
-                        .background(
-                            color = MaterialTheme.colorScheme.surface,
-                        )
+            indicator = {
+                TabRowDefaults.PrimaryIndicator(
+                    modifier = Modifier.tabIndicatorOffset(
+                        selectedTabIndex = pagerState.currentPage,
+                        matchContentSize = true
+                    ),
+                    height = 5.dp,
+                    width = Dp.Unspecified,
+                    color = MaterialTheme.colorScheme.surface
                 )
             }
         ) {
